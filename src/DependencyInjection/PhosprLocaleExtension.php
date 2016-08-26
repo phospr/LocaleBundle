5fe0cdd0dc7855ce67c25be09f3c3f687918db12
Tom Haskins-Vaughan <tom@tomhv.uk>
2016-08-25 22:20:29 -0400

    Fixing bundle

--- src/DependencyInjection/PhosprLocaleExtension.php
+++ src/DependencyInjection/PhosprLocaleExtension.php
@@ -29,13 +29,9 @@ class PhosprLocaleExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(
             __DIR__.'/../Resources/config'
         ));
         $loader->load('services.yml');
     }
 }
-
